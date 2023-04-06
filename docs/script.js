33fb5193c4fe67683186e21273f37aba7da7beb6
LaTonyaJ <latonya.johnson0921@gmail.com>
2023-04-05 19:04:23 -0500

    searchMenu

--- docs/script.js
+++ docs/script.js
@@ -1,53 +1,77 @@
-(function (){
+(function () {
     'use strict';
 
-    angular.module('ShoppingListCheckOff', [])
-    .controller('ToBuyController', ToBuyController)
-    .controller('AlreadyBoughtController', AlreadyBoughtController)
-    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownControllerApp', NarrowItDownControllerApp)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItems);
 
-    ToBuyController.$inject = ['ShoppingListCheckOffService'];
-    function ToBuyController(ShoppingListCheckOffService){
-        var buyItem = this;
-
-        buyItem.items = ShoppingListCheckOffService.getItems();
-        buyItem.buy = function (idx) {
-            ShoppingListCheckOffService.purchase(idx);
+    function FoundItems(){
+        var ddo = {
+        templateUrl: 'found_items.html',
+        scope: {
+            itemsFound: "<",
+            onRemove: "&"
         }
-    };
-
-    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-    function AlreadyBoughtController(ShoppingListCheckOffService){
-        var bought = this;
-
-        bought.items = ShoppingListCheckOffService.boughtAlready();
-
-    };
-
-    function ShoppingListCheckOffService(){
-        var service = this;
-
-        var buy_list = [
-            {name: "Cookies", quantity: 5},
-            {name: "Milk", quantity: 1 },
-            {name: "Chips", quantity: 10},
-            {name: "Bottled Water", quantity: 32},
-            {name: "Apples", quantity: 6}
-        ];
-        var bought_list = [];
-
-        service.purchase = function (idx) {
-            var item = buy_list.splice(idx, 1);
-            bought_list.push(...item);
         };
+        return ddo;
+    }
 
-        service.getItems = function() {
-            return buy_list;
+
+    NarrowItDownControllerApp.$inject = ['MenuSearchService', '$scope'];
+    function NarrowItDownControllerApp(MenuSearchService, $scope){
+        var narrow = this;
+        var unfiltered = [];
+        var items = [];
+
+        
+        narrow.searchMenu = function () {
+            MenuSearchService.getMatchedMenuItems($scope.search)
+
+            .then((response) => {
+            for (const key in response.data){
+                unfiltered.push(response.data[key].menu_items)
+            };
+                        
+            for (let i = 0; i < unfiltered.length; i++){
+                for(let j = 0; j < unfiltered[i].length; j++){
+                    if(unfiltered[i][j].description.includes($scope.search)){
+                        var item = {
+                            name: unfiltered[i][j].name,
+                            short_name: unfiltered[i][j].short_name,
+                            desc: unfiltered[i][j].description
+                        }
+                        items.push(item)
+                    };
+                }
+            }
+
+                narrow.found = items;
+                console.log(narrow.found)
+            })
+            .catch((error) =>{
+                console.log('error: ' + error)
+            })
         }
 
-        service.boughtAlready = function () {
-            return bought_list;
+        narrow.remove = function ($index) {
+            narrow.found.splice($index, 1);
         }
+
     };
 
+
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http){
+        var search = this;
+        
+        search.getMatchedMenuItems = function () {
+            var resp = $http({
+                method: "GET",
+                url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json"
+            });
+
+            return resp; 
+        }
+    }
 })();
